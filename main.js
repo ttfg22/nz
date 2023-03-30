b0134285d2841430443632415ada6bb30e1c0301
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-03-30 15:52:35 +0200

    added more varibales

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 //die Latitude und Longitude des Stops werden in Varibalen geschrieben
 let stop_lat = -43.5;
 let stop_long = 170.22;
+let zoom_factor = 13
+let title = 'Der Franz-Joseph Gletscher'
 
-let map = L.map('map').setView([stop_lat, stop_long], 11); // Hier wird auf die Leaflet Map verwiesen; 13 steht für den z-Wert um den hinaus gezoomt wird-->
+//die map Variable wird festgelegt 
+let map = L.map('map').setView([stop_lat, stop_long], zoom_factor); // Hier wird auf die Leaflet Map verwiesen; 13 steht für den z-Wert um den hinaus gezoomt wird-->
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map); //hier wird die Hintergrundkarte eingestellt
 
 L.marker([stop_lat, stop_long]).addTo(map) //der Marker wird gesetzt
-    .bindPopup('Der Franz-Joseph Gletscher') //das Pop Up wird erzeugt
+    .bindPopup(title) //das Pop Up wird erzeugt
     .openPopup();   //das Pop Up wird direkt geöffnet
