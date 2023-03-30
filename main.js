c9f8adb9a3775d5422b25110b964385616b5ee87
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-03-30 15:46:43 +0200

    added latitude und longitude varibales

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var map = L.map('map').setView([-43.5, 170.22], 11); // Hier wird auf die Leaflet Map verwiesen; 13 steht für den z-Wert um den hinaus gezoomt wird-->
+//die Latitude und Longitude des Stops werden in Varibalen geschrieben
+let stop_lat = -43.5;
+let stop_long = 170.22;
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map); //hier wird die Hintergrundkarte eingestellt
+let map = L.map('map').setView([stop_lat, stop_long], 11); // Hier wird auf die Leaflet Map verwiesen; 13 steht für den z-Wert um den hinaus gezoomt wird-->
 
-        L.marker([-43.5, 170.22]).addTo(map) //der Marker wird gesetzt
-            .bindPopup('Der Franz-Joseph Gletscher') //das Pop Up wird erzeugt
-            .openPopup();   //das Pop Up wird direkt geöffnet
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map); //hier wird die Hintergrundkarte eingestellt
+
+L.marker([stop_lat, stop_long]).addTo(map) //der Marker wird gesetzt
+    .bindPopup('Der Franz-Joseph Gletscher') //das Pop Up wird erzeugt
+    .openPopup();   //das Pop Up wird direkt geöffnet
