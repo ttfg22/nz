407eb4bb220ac1a565fe8876ee055c9d9d4329b3
Tashi Freyer <tashi.freyer@student.uibk.ac.at>
2023-03-30 16:07:36 +0200

    added array 'stops'

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ let stop_long = 170.22;
 let zoom_factor = 13
 let title = 'Der Franz-Joseph Gletscher'
 
+const STOPS = ['Fox Gletscher','Der Franz-Joseph Gletscher','Abel Tasman Nationalpark'];
+console.log(STOPS);
+console.log(STOPS[0]);
+console.log(STOPS[1]);
+console.log(STOPS[2]);
+console.log(STOPS.length)
+
+
 //die map Variable wird festgelegt 
 let map = L.map('map').setView([stop_lat, stop_long], zoom_factor); // Hier wird auf die Leaflet Map verwiesen; 13 steht für den z-Wert um den hinaus gezoomt wird-->
 
